6b8f5ec6b51d5aff8ea2f672a48f51b75c1c62c5
jeremydmiller <jeremydmiller@yahoo.com>
2015-12-20 14:11:15 -0600

    laying out the code tree

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyDescription("Metaprogramming with Roslyn")]
+[assembly: AssemblyProduct("Lamar")]
+[assembly: AssemblyCopyright("Copyright 2015 Jeremy D. Miller et al. All rights reserved.")]
+[assembly: AssemblyTrademark("9d006d1eeccb9bc6d9006ee9a79c8f5e18bb8def")]
+[assembly: AssemblyVersion("0.5.0.51408")]
+[assembly: AssemblyFileVersion("0.5.0.51408")]
+[assembly: AssemblyInformationalVersion("0.5.0.51408")]
---
+++ src/Lamar.Testing/Class1Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+using Xunit;
+
+namespace Lamar.Testing
+{
+    public class Class1Tests
+    {
+        [Fact]
+        public void NAME()
+        {
+            1.ShouldBe(1);
+        }
+    }
+}
